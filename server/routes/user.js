43bc9807ba958c1514100b36ecc18185f42a89e2
uzair <uzairkhurshid12@gmail.com>
2020-04-11 18:56:34 +0500

    2 commit

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,9 @@
-const express=require('express')
-const router=new express.Router()
+const express = require('express')
+const router = new express.Router()
 
 
-router.get('/userIndex',(req,res)=>{
-    res.render('user/index',{
-        title:'User Dashboard'
-      })
+router.get('/userIndex', (req, res) => {
+    res.render('user/index', {
+        title: 'User Dashboard'
+    })
 })
-
-
-
-module.exports=router
